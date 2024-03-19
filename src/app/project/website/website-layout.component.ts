20fd80081cbc29cfbfbab3a0d3cb4ab484aa221a
Sasan Karimi <Sasankarimi92@gmail.com>
2024-03-19 22:11:14 +0330

    first commit

---
+++ src/app/project/website/website-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './website-layout.component.html',
+  styleUrl: './website-layout.component.css'
+})
+export class WebsiteLayoutComponent {
+
+}
