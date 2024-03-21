b78ab41fbbf3259bfa0e24d97be7dc528fb57928
Sasan Karimi <Sasankarimi92@gmail.com>
2024-03-21 13:42:30 +0330

    HomeComponent LoginComponent SignUpComponent ContactUsComponent AboutUsComponent
    added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,35 @@
 import { Routes } from '@angular/router';
+import {WebsiteLayoutComponent} from "./project/website/website-layout.component";
+import {HomeComponent} from "./project/website/home/home.component";
+import {LoginComponent} from "./project/website/login/login.component";
+import {SignUpComponent} from "./project/website/sign-up/sign-up.component";
+import {ContactUsComponent} from "./project/website/contact-us/contact-us.component";
+import {AboutUsComponent} from "./project/website/about-us/about-us.component";
 
 export const routes: Routes = [
-  { path: '', loadChildren: () => import('./project/website/website.module').then(m => m.WebsiteModule) }
+  {
+    path: '',
+    component: WebsiteLayoutComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+  {
+    path: 'login',
+    component: WebsiteLayoutComponent,
+    children: [{ path: '', component: LoginComponent }],
+  },
+  {
+    path: 'sign-up',
+    component: WebsiteLayoutComponent,
+    children: [{ path: '', component: SignUpComponent }],
+  },
+  {
+    path: 'contact-us',
+    component: WebsiteLayoutComponent,
+    children: [{ path: '', component: ContactUsComponent }],
+  },
+  {
+    path: 'about-us',
+    component: WebsiteLayoutComponent,
+    children: [{ path: '', component: AboutUsComponent }],
+  }
 ];
