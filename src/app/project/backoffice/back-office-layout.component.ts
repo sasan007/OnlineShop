b082cec33194acb9f9d5f45f4cadae23925472f9
Sasan Karimi <Sasankarimi92@gmail.com>
2024-03-22 09:09:04 +0330

    css & js files were cleaned.

--- src/app/project/backoffice/back-office-layout.component.ts
+++ src/app/project/backoffice/back-office-layout.component.ts
@@ -7,27 +7,30 @@ import {AfterViewInit, Component, Renderer2} from '@angular/core';
   templateUrl: './back-office-layout.component.html',
   styleUrl: './back-office-layout.component.css'
 })
-export class BackOfficeLayoutComponent implements AfterViewInit{
+export class BackOfficeLayoutComponent  implements AfterViewInit {
   jsFiles: string[] = [
-    '../../assets/js/jquery-3.6.0.min.js',
-    '../../assets/js/jquery-ui.min.js',
-    '../../assets/js/bootstrap/bootstrap.bundle.min.js',
-    '../../assets/js/bootstrap/bootstrap-notify.min.js',
-    '../../assets/js/bootstrap/popper.min.js',
-    '../../assets/js/feather/feather.min.js',
-    '../../assets/js/feather/feather-icon.js',
-    '../../assets/js/lazysizes.min.js',
-    '../../assets/js/slick/slick.js',
-    '../../assets/js/slick/slick-animation.min.js',
-    '../../assets/js/slick/custom_slick.js',
-    '../../assets/js/auto-height.js',
-    '../../assets/js/timer1.js',
-    '../../assets/js/fly-cart.js',
-    '../../assets/js/quantity-2.js',
-    '../../assets/js/wow.min.js',
-    '../../assets/js/custom-wow.js',
-    '../../assets/js/script.js',
-    '../../assets/js/theme-setting.js'];
+    "../../assets/back/js/jquery-3.6.0.min.js",
+      "../../assets/back/js/bootstrap/bootstrap.bundle.min.js",
+      "../../assets/back/js/icons/feather-icon/feather.min.js",
+      "../../assets/back/js/icons/feather-icon/feather-icon.js",
+      "../../assets/back/js/scrollbar/simplebar.js",
+      "../../assets/back/js/scrollbar/custom.js",
+      "../../assets/back/js/config.js",
+      "../../assets/back/js/tooltip-init.js",
+      "../../assets/back/js/sidebar-menu.js",
+      "../../assets/back/js/notify/bootstrap-notify.min.js",
+      "../../assets/back/js/notify/index.js",
+      "../../assets/back/js/chart/apex-chart/apex-chart1.js",
+      "../../assets/back/js/chart/apex-chart/moment.min.js",
+      "../../assets/back/js/chart/apex-chart/apex-chart.js",
+      "../../assets/back/js/chart/apex-chart/stock-prices.js",
+      "../../assets/back/js/chart/apex-chart/chart-custom1.js",
+      "../../assets/back/js/slick.min.js",
+      "../../assets/back/js/custom-slick.js",
+      "../../assets/back/js/customizer.js",
+      "../../assets/back/js/ratio.js",
+      "../../assets/back/js/sidebareffect.js",
+      "../../assets/back/js/script.js"];
   isLoaderShow: any = true;
 
   loadScript(index: number) {
@@ -43,11 +46,43 @@ export class BackOfficeLayoutComponent implements AfterViewInit{
     }
   }
 
+  loadCssFiles(): void {
+    const cssFiles = [
+      '../../assets/back/css/linearicon.css',
+      '../../assets/back/css/vendors/font-awesome.css',
+      '../../assets/back/css/vendors/themify.css',
+      '../../assets/back/css/ratio.css',
+      '../../assets/back/css/remixicon.css',
+      '../../assets/back/css/vendors/feather-icon.css',
+      '../../assets/back/css/vendors/scrollbar.css',
+      '../../assets/back/css/vendors/animate.css',
+      '../../assets/back/css/vendors/bootstrap.css',
+      '../../assets/back/css/vector-map.css',
+      '../../assets/back/css/vendors/slick.css',
+      '../../assets/back/css/style.css',
+      '../../assets/back/css/font.css',
+      '../../assets/back/css/custom.css'
+    ];
+
+    cssFiles.forEach(cssFile => {
+      const linkElement = this.renderer.createElement('link');
+      this.renderer.setAttribute(linkElement, 'rel', 'stylesheet');
+      this.renderer.setAttribute(linkElement, 'type', 'text/css');
+      this.renderer.setAttribute(linkElement, 'href', cssFile);
+      this.renderer.appendChild(document.head, linkElement);
+    });
+  }
+
   constructor(private renderer: Renderer2) {
+    this.loadCssFiles();
+    this.loadScript(0);
 
   }
 
   ngAfterViewInit(): void {
-    this.loadScript(0);
+    this.renderer.listen('window', 'load', () => {
+      this.isLoaderShow = false;
+    });
+
   }
 }
